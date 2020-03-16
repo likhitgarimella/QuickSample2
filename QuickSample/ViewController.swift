30a7d9a1278b710d6862bbfb4e7797f696d9d5f0
Likhit <likhitgmella@gmail.com>
2020-03-16 12:34:40 +0530

    Kart 2

---
+++ QuickSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuickSample
+//
+//  Created by Likhit Garimella on 15/10/19.
+//  Copyright © 2019 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
